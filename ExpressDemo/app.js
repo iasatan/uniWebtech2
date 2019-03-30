52163a7219f28fbefc862de89a7302e46a2a06be
Adam Satan <satanadam@outlook.com>
2019-03-30 08:47:56 +0100

    basic express server

---
+++ ExpressDemo/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port);
+
